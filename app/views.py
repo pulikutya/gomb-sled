1ac00cc2047f790c9e92b6e6ee1305c469944f89
Márton Balogh <balogh.marton.21f@szlgbp.hu>
2024-05-28 13:36:00 +0200

    dhsrfh

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from RPi import GPIO as GPIO
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17,GPIO.OUT)
+# Create your views here.
+
+def led1(request):
+    if request.POST["state"] == "on":
+        GPIO.output(17,GPIO.HIGH)
+    elif request.POST["state"] == "off":
+        GPIO.output(17,GPIO.LOW)
+
+def led0(request):
+    return render(request,"led.html",{})
