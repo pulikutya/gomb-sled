9130d225896eefed65e0a0f46f73fedf776268ef
Márton Balogh <balogh.marton.21f@szlgbp.hu>
2024-05-28 14:05:58 +0200

    fgfdg

--- gombésled/settings.py
+++ gombésled/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import local_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-36en#e!g@5x-6!8x*ig5i@m(h+s7a)-+-1!2glz$#+3ki+ibbi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = local_settings.ALLOWED_HOSTS
 
 
 # Application definition
