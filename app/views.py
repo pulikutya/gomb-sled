0c212b0fed6b9d9d2fe613a85d00320751194edd
Márton Balogh <balogh.marton.21f@szlgbp.hu>
2024-05-30 10:38:12 +0200

    grz45z

--- app/views.py
+++ app/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from RPi import GPIO as GPIO
+from django.http import JsonResponse
 
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(17,GPIO.OUT)
 # Create your views here.
 
-def led1(request):
+def ledpost(request):
     if request.POST["state"] == "on":
         GPIO.output(17,GPIO.HIGH)
     elif request.POST["state"] == "off":
         GPIO.output(17,GPIO.LOW)
     print(request.POST["state"])
+    return JsonResponse({'üres':'a pride ot be kell tiltani'})
 
 def led0(request):
     return render(request,"led.html",{})
