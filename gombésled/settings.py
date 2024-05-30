5d0eeb661b47ea6d8b78843729f10c43b4897cc1
Márton Balogh <balogh.marton.21f@szlgbp.hu>
2024-05-30 10:45:32 +0200

    d

--- gombésled/settings.py
+++ gombésled/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'gombésled.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
